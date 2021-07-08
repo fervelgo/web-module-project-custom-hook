9fcdfd7575cd0d63d7eeb7c23d29f6ac11d16fed
Fernando Velasco <fervelgo@gmail.com>
2021-07-08 18:57:45 -0500

    local storage set

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+const useLocalStorage = (key, initialValue) => {
+    
+    const [storedValue, setStoredValue] = useState(() => {
+        // if(localStorage.getItem(key)) {
+        //     return JSON.parse(localStorage.getItem(key));
+        // } else {
+        //     localStorage.setItem(key, initialValue);
+        //     return initialValue;
+        // }
+
+        const item = window.localStorage.getItem(key);
+        return item ? JSON.parse(item) : initialValue;
+    });
+
+    const setValue = (value) => {
+        window.localStorage.setItem(key, value);
+        setStoredValue(value);
+    }
+
+    return [storedValue, setValue];
+
+};
